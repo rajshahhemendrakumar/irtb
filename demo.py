7d4647b70ab29798a370269a2b6642d06604567b
rajshahhemendrakumar <154873315+rajshahhemendrakumar@users.noreply.github.com>
2023-12-26 13:37:30 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,81 @@
+name = "raj"
+age = 21
+
+name ="xyz"
+age = 22
+
+
+name ="bf"
+age = 23
+
+is_raj = False
+print( name )
+
+print( age)
+
+
+first_name = "Tony"
+last_name = "shark"
+age = 51
+
+
+is_genius = False
+
+       
+
+ 
+name = input("what is superhero?")
+
+print(name)
+
+old_age = input("enter your old age:")
+
+new_age = int (old_age) + 2
+print (new_age)
+
+
+first_name=input("enter first number:")
+second_name=input("enter a second number:")
+sum =  int(first_name) + int(second_name)
+
+print(sum)
+
+var =input("enter A number value.......")
+var1=input("enter B number value.......")
+
+sum = int(var) + int(var1)
+
+
+print(sum)
+
+
+
+
+
+name = "tony stark"
+print(name.lower())
+print(name)
+
+
+name = " raj shah"
+
+print(name.find('s'))
+
+
+name = "raj shah"
+
+print(name.replace("raj shah","shah raj"))
+
+
+
+name = "rah"
+
+print('h'in name)
+
+
+
+
+
+
+ 
+
